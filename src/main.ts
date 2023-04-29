d09d7cc717ac1e4ca38fb5927d4a2426b57832db
aimeliala <idstack@outlook.co.id>
2023-04-29 19:00:15 +0700

    starting up a nest app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 @Controller()
 class AppController {
@@ -7,3 +8,15 @@ class AppController {
 		return 'Hello World!';
 	}
 }
+
+@Module({
+	controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	await app.listen(3000);
+}
+
+bootstrap();
