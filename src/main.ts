ad163336d38b9bdbb5c9cba1b5724b38d7a579d9
aimeliala <idstack@outlook.co.id>
2023-04-29 19:24:26 +0700

    file naming conventions

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,5 @@
-import { Controller, Module, Get } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
-@Controller()
-class AppController {
-	@Get()
-	getRootRoute() {
-		return 'Hello World!';
-	}
-}
-
-@Module({
-	controllers: [AppController],
-})
-class AppModule {}
+import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
